f182fc14c52661db16d722b702ba72ef7a24d9fb
Lee Pedersen <leepedersenr@gmail.com>
2020-01-17 12:48:32 -0800

    final commit on this dumpster fire. Switching to epicodus computer

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :product
 
+  # dry code a bit here?
+  validates :author, presence: true
+  validates :rating, presence: true
+  validates :content_body, presence: true
+
   before_save(:titleize_reviewer_name)
 
   private
